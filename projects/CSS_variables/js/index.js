31da05027f2085bee62291771da9cff13abd5844
Marat Valiev <mz.valiev@gmail.com>
2018-03-04 23:18:21 -0500

    Added new project and updated index.html

---
+++ projects/CSS_variables/js/index.js
@@ -0,0 +1,12 @@
+//Change transform values of the box element
+const inputs = Array.from($('.controls input'));
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  console.log(suffix);
+  $('.box').css(`--${this.name}`, this.value + suffix);
+  $(this).next().html(this.value + suffix);
+}
+
+inputs.forEach(input => $(input).on('change', handleUpdate));
+inputs.forEach(input => $(input).on('mousemove', handleUpdate));
